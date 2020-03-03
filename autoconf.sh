158d2dcec3aebc66619450e02e596505fceef718
98PASS <pedroantoniodesouzasilva@gmail.com>
2020-03-03 12:31:50 -0300

    Adicionando os Primeiros Programas Essenciais

---
+++ autoconf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get update; sudo apt-get upgrade -y;
+
+sudo apt install net-tools -y;
+sudo apt install ubuntu-make -y;
+sudo apt install ssh -y;
+sudo apt install  mariadb  -y;
+sudo apt install git -y;
+sudo apt install default-jdk -y;
+sudo snap install gimp;
+
+
+#cd ~/Downloads
+
+sudo apt-get upgrade -y
