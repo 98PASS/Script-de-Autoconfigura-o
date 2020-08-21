16c8fedff0c35dab71468a138e2303009eef60eb
98PASS <pedroantoniodesouzasilva@gmail.com>
2020-08-21 05:01:23 -0300

    Mais ajustes para ubuntu

--- autoconf.sh
+++ autoconf.sh
@@ -11,8 +11,8 @@ for pacote in `cat snap.txt` ; do sudo snap install $pacote; done;
 echo -e "\033[0;34m""\nInstalando pacotes do PIP para Python3:  \033[0m"
 #for pacote in `cat pip3.txt` ; do pip3 install $pacote; done;
 
-cd ~/Downloads/
-wget https://downloads.tuxfamily.org/godotengine/3.2.2/Godot_v3.2.2-stable_x11.64.zip
-
 sudo apt-get upgrade -fy;
 echo -e "\033[0;32m""\nAcabou! Seus Programas Estão Prontos :)\n"
+
+cd ~/Downloads/
+wget https://downloads.tuxfamily.org/godotengine/3.2.2/Godot_v3.2.2-stable_x11.64.zip
