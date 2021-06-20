d15e2eb7fc216c263bf95cfaf82cb895df82b4f9
98PASS <panssfancy@gmail.com>
2021-06-20 14:23:54 -0300

    Adicionando os pactoes corretos + help para apache

--- autoconf.sh
+++ autoconf.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-cd ~/Downloads/
+sudo echo -e "Baixando Itens da Internet"
 
-echo -e "Baixando Itens da Internet"
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
 
-sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin &
-
-wget -O ~/Downloads/Drupal_Latest.zip https://www.drupal.org/download-latest/zip &
+wget -O ~/Downloads/Drupal_Latest.zip https://www.drupal.org/download-latest/zip
 #baixa a versão mais recente do Drupal
-
-sudo apt-get update;
 
 echo -e "\033[0;33m""\nInstalando pacotes SNAP:  \033[0m"
 for pacote in `cat snap.txt` ; do sudo snap install $pacote; done;
@@ -17,6 +13,7 @@ for pacote in `cat snap.txt` ; do sudo snap install $pacote; done;
 echo -e "\033[0;34m""\nInstalando pacotes do PIP para Python3:  \033[0m"
 #for pacote in `cat pip3.txt` ; do pip3 install $pacote; done;
 
+sudo apt-get update;
 echo -e "\033[0;36m""\nInstalando pacotes APT: \033[0m"
 for pacote in `cat apt.txt` ; do sudo apt install $pacote -y ; done;
 sudo ufw allow in "Apache Full" #permite trafego http e https para o Apache
